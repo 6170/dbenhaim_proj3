b118eac9e2f875903c72396cb8c467fbcc754ca3
David Benhaim <davidbenhaim12@gmail.com>
2012-11-05 00:42:11 -0500

    no more confirmations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
-
+         :recoverable, :rememberable, :trackable, :validatable
   ## Database authenticatable
   field :email,              :type => String, :default => ""
   field :encrypted_password, :type => String, :default => ""
